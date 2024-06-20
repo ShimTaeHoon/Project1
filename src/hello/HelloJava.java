81ebdad22d23a1f813cc3f18ac4ac68c83ac95df
G203 <G203@DESKTOP-JHI7NMB>
2024-06-20 15:57:02 +0900

    첫 프로젝트 생성

---
+++ src/hello/HelloJava.java
@@ -0,0 +1,23 @@
+package hello;
+
+/*
+ * 작성일자 : 2024-06-20
+ * 작성자 : 심태훈
+ * ctrl + shift + (+/-) 폰트 크기 변경(일시적)
+ * 자동 완성 -> ctrl + space
+ * Project -> Build Automatically 체크되면 자동으로 컴파일
+ * workspace의 bin폴더는 바이트코드로 컴파일 된 것(.class)
+ * 			   실제로 이클립스가 실행하는 건 class
+ * 			   src폴더는 소스코드(.java)
+ * */
+
+public class HelloJava {
+
+	public static void main(String[] args) {
+	
+		System.out.println("shimtaehoon");
+		
+	}
+
+	
+}
